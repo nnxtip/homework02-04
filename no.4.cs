b48a2173c1dc6cea6d22d1839531b640b4d897dd
nnxtip <123537484+nnxtip@users.noreply.github.com>
2023-05-19 22:45:27 +0700

    Add files via upload

---
+++ no.4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class MainClass {
+    public static void Main(string[] args) {
+        string[] input = Console.ReadLine().Split(' ');
+        int N = int.Parse(input[0]);
+        int K = int.Parse(input[1]);
+
+        input = Console.ReadLine().Split(' ');
+        int[] population = new int[N];
+        for (int i = 0; i < N; i++) {
+            population[i] = int.Parse(input[i]);
+        }
+
+        int maxCustomers = GetMaxCustomers(population, N, K);
+        Console.WriteLine(maxCustomers);
+    }
+
+    static int GetMaxCustomers(int[] population, int N, int K) {
+        int maxCustomers = 0;
+        
+        for (int i = 0; i < N; i++) {
+            int customers = 0;
+            
+            for (int j = i - K; j <= i + K; j++) {
+                if (j >= 0 && j < N) {
+                    customers += population[j];
+                }
+            }
+            
+            if (customers > maxCustomers) {
+                maxCustomers = customers;
+            }
+        }
+        
+        return maxCustomers;
+    }
+}
